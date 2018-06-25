b074dbeae7cf04cb3700f0fe134a237013f9f211
Evan Wheeler <anon0mys87@gmail.com>
2018-06-24 23:03:02 -0600

    Completed event listener and JQuery css updating

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit(function(event) {
+    event.preventDefault()
+    style_page();
+  })
 });
+
+const style_page = () => {
+  let selector = $("input[name = 'selector']").val()
+  let property = $("input[name = 'property']").val()
+  let value = $("input[name = 'value']").val()
+  $(selector).css(property, value)
+}
